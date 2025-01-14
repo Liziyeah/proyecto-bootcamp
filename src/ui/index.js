b427d7ef6432af4b13a6eeb96be7f2b626613909
Liziyeah <marianitalizcanor@gmail.com>
2025-01-14 16:37:18 -0500

    modal

---
+++ src/ui/index.js
@@ -0,0 +1,20 @@
+const gallery = document.querySelector('.event-gallery');
+const modal = document.getElementById('imageModal');
+const modalImage = document.getElementById('modalImage');
+
+gallery.addEventListener('click', (e) => {
+	const clickedImage = e.target.closest('.card');
+	if (clickedImage) {
+		const image = clickedImage.querySelector('img');
+		if (image) {
+			modalImage.src = image.src;
+			modal.style.display = 'flex';
+		}
+	}
+});
+
+window.addEventListener('click', (e) => {
+	if (e.target === modal) {
+		modal.style.display = 'none';
+	}
+});
